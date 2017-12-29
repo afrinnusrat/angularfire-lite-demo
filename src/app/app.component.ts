43d0efef8227d09343573a332369434c5d45c279
Hamed <mouyahama@gmail.com>
2017-12-30 00:30:44 +0300

    Angularfire Lite v5.1 release demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { AngularFireLiteAuth, AngularFireLiteDatabase, AngularFireLiteFirestore } from 'angularfire-lite';
+import {AngularFireLiteAuth, AngularFireLiteDatabase, AngularFireLiteFirestore} from 'angularfire-lite';
 
-import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(public db: AngularFireLiteDatabase,
-              public auth: AngularFireLiteAuth,
-              public store: AngularFireLiteFirestore) {
-  }
+    constructor(public db: AngularFireLiteDatabase,
+                public auth: AngularFireLiteAuth,
+                public firestore: AngularFireLiteFirestore) {
+    }
 
-  database;
-  databaseData;
+    databaseData;
+    databaseList;
+    databaseQuery;
 
+    firestoreData;
+    firestoreList;
+    firestoreQuery;
 
-  firestore;
-  firestoreData;
-
-  authState;
-
-
-  ngOnInit() {
-
-    // Realtime Database
-    this.database = this.db.read('hello/hello');
-    this.database.subscribe((data) => {
-      this.databaseData = data;
-    });
-
-    // Firestore
-    this.firestore = this.store.read('hello/hellodoc');
-    this.firestore.subscribe((data) => {
-      this.firestoreData = data;
-    });
-
-    // Authentication
-    this.auth.isAuthenticated().subscribe((isAuth) => {
-      this.authState = isAuth;
-    });
+    authState;
 
 
-  }
+    ngOnInit() {
 
-  // Login Button Clicked
-  login() {
-    this.auth.signin('test@gmail.com', '123456');
-  }
+        // Realtime Database
+        this.db.read('hello/hello').subscribe((data) => {
+            this.databaseData = data;
+        });
+
+
+        // Realtime Database list retrieval
+        this.databaseList = this.db.read('hello');
+
+
+        // Realtime Database query
+        this.db.query('hello').limitToLast(1).orderByKey().on('value').subscribe((data) => {
+            this.databaseQuery = data;
+        });
+
+        // Firestore
+        this.firestore.read('hello/hellodoc').subscribe((data) => {
+            this.firestoreData = data;
+        });
+
+        // Firestore  list retrieval
+        this.firestoreList = this.firestore.read('hello');
+
+        // Firestore Query
+        this.firestore.query('query').limit(1).on().subscribe((data) => {
+            this.firestoreQuery = data;
+        });
+
+        // Firestore batched writes
+        this.firestore.batch()
+            .set('batchCollection/batchDoc', {set: 'this is a batch set'})
+            .update('batchCollection/batchDoc', {update: 'this is a batch update'})
+            .commit()
+            .subscribe(() => {
+                console.log('batched writes complete');
+            });
+
+        // Firestore transaction
+        this.firestore.transaction().get('hello/hellodoc').subscribe((payload1) => {
+            console.log(payload1.data);
+            payload1.next.get('batchHello/batchDoc').subscribe((payload2) => {
+                console.log(payload2.data);
+                payload2.next
+                    .set('transaction/tranDoc', {hellofromTrans: 'this is a set from the transaction'})
+                    .run()
+                    .subscribe(() => console.log('transaction complete'));
+            });
+        });
+
+
+        // Authentication
+        this.auth.isAuthenticated().subscribe((isAuth) => {
+            this.authState = isAuth;
+        });
+
+
+    }
+
+    // Login Button Clicked
+    login() {
+        this.auth.signin('test@gmail.com', '123456');
+    }
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireLite } from 'angularfire-lite';
 import {environment} from '../environments/environment';
-import {HttpClientModule} from '@angular/common/http';
-
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFireLite.forRoot(environment.config),
-    BrowserModule.withServerTransition({appId: 'angularfire-lite-demo'}),
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        AngularFireLite.forRoot(environment.config),
+        BrowserModule.withServerTransition({appId: 'angularfire-lite-demo'}),
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
 
