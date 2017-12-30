859251c5d65344af55501665a10f8abdd07568db
Hamed Baatour <hamedbaatour@gmail.com>
2017-12-30 23:58:19 +0300

    Angularfire-lite v5.1.2 demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AngularFireLite } from 'angularfire-lite';
+import {AppComponent} from './app.component';
+import {AngularFireLite} from 'angularfire-lite';
 import {environment} from '../environments/environment';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        AngularFireLite.forRoot(environment.config),
-        BrowserModule.withServerTransition({appId: 'angularfire-lite-demo'}),
-    ],
-    providers: [
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AngularFireLite.forRoot(environment.config),
+    BrowserModule.withServerTransition({appId: 'angularfire-lite-demo'}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
 
