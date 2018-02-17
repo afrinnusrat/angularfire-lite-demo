1f285dc8a80b455788cd7760f73b558881e4190f
Hamed Baatour <hamedbaatour@gmail.com>
2018-02-17 16:28:01 +0300

    adding config notice to get started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {AngularFireLite} from 'angularfire-lite';
-import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    AngularFireLite.forRoot(environment.config),
+    AngularFireLite.forRoot({
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: '',
+      messagingSenderId: ''
+    }),
     BrowserModule.withServerTransition({appId: 'angularfire-lite-demo'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+  constructor() {
+    console.warn('Please add your own Firebase project configuration in app.module.ts');
+  }
 }
-
-
-
 
